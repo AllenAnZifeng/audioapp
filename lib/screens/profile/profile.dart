e824c33e7868df603562d8d3331159f8cb4467b7
Allen <zifeng.allen@gmail.com>
2023-04-03 18:01:15 -0400

    basic profile functionality

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -15,8 +15,10 @@ import '../../models/appUser.dart';
 import '../../services/auth.dart';
 
 
+
 class _LineChart extends StatelessWidget {
   final Map<String, dynamic> data;
+
   const _LineChart({Key? key, required this.data}) : super(key: key);
 
   Widget leftTitleWidgets(double value, TitleMeta meta) {
@@ -25,9 +27,8 @@ class _LineChart extends StatelessWidget {
     );
     return SideTitleWidget(
       axisSide: meta.axisSide,
-      space: 10,
-      child: Text(meta.formattedValue, style: style),
-
+      space: 8,
+      child: Text(value.toStringAsFixed(2), style: style),
     );
   }
 
@@ -42,94 +43,132 @@ class _LineChart extends StatelessWidget {
     );
   }
 
+
+
+
   @override
   Widget build(BuildContext context) {
     print('data $data');
 
-    List<FlSpot> spots = [];
+    List<FlSpot> leftSpots = [];
+    List<FlSpot> rightSpots = [];
 
-    List<String> frequencies = ['500', '1000', '2000', '3000', '4000', '6000', '8000'];
+
+    List<String> frequencies = [
+      '500',
+      '1000',
+      '2000',
+      '3000',
+      '4000',
+      '6000',
+      '8000'
+    ];
     for (String frequency in frequencies) {
-
-      spots.add(FlSpot(double.parse(frequency), data[frequency][0].toDouble()));
-
+      leftSpots
+          .add(FlSpot(double.parse(frequency), data[frequency][0].toDouble()));
+      rightSpots
+          .add(FlSpot(double.parse(frequency), data[frequency][1].toDouble()));
     }
 
+    const double opacity = 1.0;
 
+    final LineChartBarData leftEarLineData = LineChartBarData(
+      spots: leftSpots,
+      color: Colors.purple[100],
+      barWidth: 3,
+      isStrokeCapRound: true,
+      dotData: FlDotData(
+        show: true,
+          getDotPainter: (spot, percent, barData, index) {
+            return FlDotCirclePainter(
+              radius: 4,
+              color: Colors.purple[100]?.withOpacity(opacity),
+              strokeWidth: 6,
+              strokeColor: Colors.purple[100]?.withOpacity(opacity),
+            );
+          }
+      ),
+    );
+
+    final LineChartBarData rightEarLineData = LineChartBarData(
+      spots: rightSpots,
+      color: Colors.green[100],
+      barWidth: 3,
+      dotData: FlDotData(
+        show: true,
+        getDotPainter: (spot, percent, barData, index) {
+          return FlDotCirclePainter(
+            radius: 4,
+            color: Colors.green[100]?.withOpacity(opacity),
+            strokeWidth: 2,
+            strokeColor: Colors.green[100]?.withOpacity(opacity),
+          );
+        }
+      ),
+    );
 
     return LineChart(
       LineChartData(
         lineTouchData: LineTouchData(
           handleBuiltInTouches: true,
           touchTooltipData: LineTouchTooltipData(
-            tooltipBgColor: Colors.blueGrey.withOpacity(0.5),
+            tooltipBgColor: Colors.blueGrey.withOpacity(0.7),
           ),
         ),
         titlesData: FlTitlesData(
-            bottomTitles: AxisTitles(
-              axisNameSize: 20,
-              axisNameWidget: const Text(
-                'Frequency',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              sideTitles: SideTitles(
-                showTitles: true,
-                getTitlesWidget: (value, meta) =>
-                    bottomTitleWidgets(value, meta),
-                reservedSize: 40,
+          topTitles: AxisTitles(
+            sideTitles: SideTitles(showTitles: false),
+          ),
+          bottomTitles: AxisTitles(
+            axisNameSize: 20,
+            axisNameWidget: const Text(
+              'Frequency - Hz',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            rightTitles: AxisTitles(
-              sideTitles: SideTitles(showTitles: false),
-            ),
-            topTitles: AxisTitles(
-              sideTitles: SideTitles(showTitles: false),
-            ),
-            leftTitles: AxisTitles(
-              axisNameSize: 20,
-              axisNameWidget: const Text(
-                'Volume',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              sideTitles: SideTitles(
-                showTitles: true,
-                getTitlesWidget: (value, meta) =>
-                    leftTitleWidgets(value, meta),
-                reservedSize: 40,
-              ),
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: (value, meta) => bottomTitleWidgets(value, meta),
+              reservedSize: 40,
             ),
           ),
+          rightTitles: AxisTitles(
+            sideTitles: SideTitles(showTitles: false),
+          ),
+          leftTitles: AxisTitles(
+            axisNameSize: 20,
+            axisNameWidget: const Text(
+              'Volume - dB',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            sideTitles: SideTitles(
+              showTitles: true,
+              interval: 0.01,
+              getTitlesWidget: (value, meta) => leftTitleWidgets(value, meta),
+              reservedSize: 40,
+            ),
+          ),
+        ),
         lineBarsData: [
-          LineChartBarData(spots: spots,
-            color: Colors.purple[100],),
-          // LineChartBarData(spots: [
-          //   FlSpot(3, 3),
-          //   FlSpot(4, 4),
-          //   FlSpot(5, 10),
-          // ], color: Colors.blue[100],),
+          leftEarLineData,
+          rightEarLineData,
         ],
         minX: 0,
         maxX: 8000,
         minY: 0,
-        maxY: 0.1,
-
+        maxY: 0.2,
       ),
-      swapAnimationDuration: Duration(milliseconds: 150),
-      // Optional
-      swapAnimationCurve: Curves.linear, // Optional
-
-
 
     );
   }
 }
-
 
 class Profile extends StatefulWidget {
   // final Function toggleView;
@@ -144,11 +183,16 @@ class _ProfileState extends State<Profile> {
 
   late Widget content;
 
+  String getTitles(value) {
+    DateTime date = DateTime.fromMillisecondsSinceEpoch(int.parse(value));
+    return '${date.year}/${date.month}/${date.day}';
+  }
+
   @override
   Widget build(BuildContext context) {
-
     final appUser = Provider.of<AppUser?>(context);
     final appUserData = Provider.of<AppUserData?>(context);
+    String time = '';
 
     print('Profile appUser: $appUser');
     print('Profile appUserData: $appUserData');
@@ -161,12 +205,14 @@ class _ProfileState extends State<Profile> {
       setState(() {
         content = const Text('No data');
       });
-    }else {
+    } else {
+      time = getTitles( appUserData.data['test1']!.last['time']);
       setState(() {
-        content = _LineChart( data: appUserData.data['test1']!.last);
+        content = _LineChart(data: appUserData.data['test1']!.last);
       });
-
     }
+
+
 
     return Scaffold(
       appBar: AppBar(
@@ -196,9 +242,7 @@ class _ProfileState extends State<Profile> {
             ),
           ),
           TextButton.icon(
-            onPressed: () => {
-            GoRouter.of(context).go('/home')
-            },
+            onPressed: () => {GoRouter.of(context).go('/home')},
             icon: Icon(
               Icons.home,
               color: Colors.pink[50],
@@ -229,17 +273,44 @@ class _ProfileState extends State<Profile> {
               style: TextStyle(fontSize: 24.0),
             ),
           ),
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              'Audiogram $time',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16.0),
+            ),
+          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 10,
+              height: 10,
+              decoration: BoxDecoration(color: Colors.purple[100], shape: BoxShape.circle),
+            ),
+            const SizedBox(width: 4),
+            const Text('Left Ear', style: TextStyle(fontSize: 12)),
+            Container(
+              width: 10,
+              height: 10,
+              decoration: BoxDecoration(color: Colors.green[100], shape: BoxShape.circle),
+            ),
+            const SizedBox(width: 4),
+            const Text('  Right Ear', style: TextStyle(fontSize: 12)),
+          ],
+        ),
+          const SizedBox(height: 20.0, width: double.infinity),
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.only(bottom: 50,right: 16, left: 6),
+              padding: const EdgeInsets.only(
+                  top: 20, bottom: 20, right: 40, left: 20),
               child: content,
             ),
           ),
-
+          const SizedBox(height: 50.0, width: double.infinity),
         ],
       ),
     );
   }
 }
-
-
